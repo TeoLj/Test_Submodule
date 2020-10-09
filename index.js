d75b2b2505c45175e0e2643a29348e6f5350ee6b
yLjubevski <ljubevski@ysura.com>
2020-10-09 15:44:23 +0200

    add substract example

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const lib = require('./math-library/basic-library');
 const result = lib.add(3,88);
 console.log(result);
 
+//more examples
+const resultSub = lib.sub(3,88);
+console.log(resultSub);
+
